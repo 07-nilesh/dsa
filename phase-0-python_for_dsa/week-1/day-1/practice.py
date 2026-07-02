eee7dcf53033502e4f543ed6150137fa822bce59
Nilesh <kashyapnilesh0711@gmail.com>
2026-07-02 15:36:30 +0530

    day-1: practiced list as array problems

---
+++ phase-0-python_for_dsa/week-1/day-1/practice.py
@@ -0,0 +1,56 @@
+# 1.Create a 2D matrix 4x4 filled with zeros from scratch
+arr=[[0]*4 for _ in range(4)]
+print(arr)
+
+# 2.reverse a list 3 different ways (slicing, loop, built-in)
+arr1=list(range(10))
+# Using slicing
+print(arr1[::-1])
+# Using loop
+reversed_arr=[]
+for i in range(len(arr1)-1,-1,-1):
+    reversed_arr.append(arr1[i])
+print(reversed_arr)
+# Using built-in function
+arr1.reverse()
+print(arr1)
+arr1.sort(reverse=True)
+print(arr1)
+
+# 3.Sort a list of strings by their length
+strings=["apple","banana","kiwi","grape","watermelon"]
+# Using sorted with key
+sorted_strings=sorted(strings, key=len)
+print(sorted_strings)
+# Using sort with key
+strings.sort(key=len)
+print(strings)
+
+# 4.Find the second largest element in a list without sorting
+# directly finding the second largest element
+arr1=[3,5,1,8,2,7]
+
+def second_largest(arr):
+    first=second=float('-inf')
+    for num in arr:
+        if num>first:
+            #new largest found, old largest becomes second largest
+            second=first
+            first =num
+        elif first>num>second:
+            #new second largest found
+            second=num
+    return second
+
+print(second_largest(arr1))
+
+arr1=[3,5,1,8,2,7]
+first=second=float('-inf')
+for i in range(len(arr1)):
+    
+    if arr1[i]>first:
+        second=first
+        first=arr1[i]
+    elif first>arr1[i]>second:
+        second=arr1[i]
+print(second)
