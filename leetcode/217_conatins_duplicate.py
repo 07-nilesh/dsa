b9638f5e3f98e7144b21113636eda3fc3bac0f5a
Nilesh <kashyapnilesh0711@gmail.com>
2026-07-05 13:13:15 +0530

    Day-4 | leetcode | solved problem no. 217 conatins duplicate

---
+++ leetcode/217_conatins_duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findduplicate(nums: list[int]) -> bool:
+        unique=set(nums)
+        if len(unique)==len(nums):
+            return False
+        else:
+            return True
+print(Solution.findduplicate(nums=[1,2,3,1]))
+print(Solution.findduplicate(nums=[1,2,3,4]))
