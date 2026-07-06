9aac1eef65369083853882e15247ab5385ecfe88
Nilesh <kashyapnilesh0711@gmail.com>
2026-07-06 13:00:12 +0530

    Day-5 | Stack & deque | learn about stack and its operations

--- phase-0-python_for_dsa/week-1/day-5/stack.py
+++ phase-0-python_for_dsa/week-1/day-5/stack.py
@@ -1,19 +1,20 @@
+# stack - LIFO
 browser_history=[]
-# append
+
+# append to top of stack -O(1)
 browser_history.append("google.com")
 browser_history.append("github.com")
 browser_history.append("notion.so")
 print(browser_history)
 
-# peek
+# peek on top of stack - O(1)
 top_page=browser_history[-1]
 print(top_page)
 
-#pop
+#pop from stack - O(1)
 last_visited=browser_history.pop()
 print(last_visited)
 
-#Isempty
+#ask if something is in stack - O(1)
 if browser_history:
     print("True")
-    
