c5f3e944150ffc8554e109fa64303687837aa2f8
Nilesh <kashyapnilesh0711@gmail.com>
2026-07-08 18:06:37 +0530

    Day-7 | sorting algorithm | learn about quicksort

--- phase-0-python_for_dsa/week-1/day-7/sorting_algorithm/quick_sort.py
+++ phase-0-python_for_dsa/week-1/day-7/sorting_algorithm/quick_sort.py
@@ -16,3 +16,11 @@ def quick_sort(nums,low,high):
 data=[5,1,4,3,2]
 quick_sort(data,0,len(data)-1)
 print(data)
+
+"""
+⏱️ Complexity Analysis
+-> Best/Average Case Time: O(N log N) — This happens if your pivot splits the array roughly in half every single time.
+-> Worst Case Time: O(N^2) — This happens if the array is already sorted (or reversed) 
+   and you always pick the first or last element as your pivot.You end up splitting arrays 
+   into 0 elements on one side and N-1 elements on the other side,turning your tree into a long linear loop.
+"""
